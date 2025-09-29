13b5dbd829f3935b2f1c7e1fe289f5983946deab
Lívia Martins Bastos <bastlivia009@gmail.com>
2025-09-29 00:59:28 -0300

    Finalização da Atividade 03: Calculadora de Idade

---
+++ br/com/ifba/atividade01/MainApp.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.ifba.atividade01;
+
+/**
+ *
+ * @author Lívia Martins Bastos
+ */
+public class MainApp {
+    
+    public static void main(String[] args){
+        // Crio uma nova instancia da CalculadoraGUI e deico visivel para o usuario 
+        new br.com.ifba.atividade03.view.CalculadoraGUI().setVisible(true);
+    }
+}
