a3e0d674683ba5d0883f19daecee069e9d3e9742
Lívia Martins Bastos <bastlivia009@gmail.com>
2026-05-02 20:10:47 -0300

    Atividade 10: Configuração de Maven, JPA e Hibernate

---
+++ prg03persistencia_atividade10/src/main/java/br/com/ifba/prg03persistencia_atividade10/Prg03persistencia_atividade10.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.ifba.prg03persistencia_atividade10;
+
+/**
+ *
+ * @author Lívia Martins Bastos
+ */
+public class Prg03persistencia_atividade10 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
